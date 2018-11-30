86eb418d92b47aa5bcc011a491538d4521185237
Caroline Lu <42614552+carolinedlu@users.noreply.github.com>
2018-11-30 17:20:50 -0500

    Update Starter.js

--- Starter.js
+++ Starter.js
@@ -1,10 +1,15 @@
 //$(document).ready(() => {
 //};
 
-
 function buildModelsInterface() {
     emptyInterface();
     let body = $('body');
+    //Possibly have a carousel here with airplane models. For now, drop-down menu.
+    body.append('<h1>Select an airplane model:</h1>');
+    //This section should dynamically generate choices based on the airplane models that are in the database. For now, static options.
+    body.append('<select><option value="ModelA">Model A</option><option value="ModelB">Model B</option><option value="ModelC">Model C</option><option value="ModelD">Model D</option></select><br><br>');
+
+
     body.append('<button onclick="buildDestinationsInterface()">Destinations</button>');
     body.append('<button onclick="buildMileageInterface()">Mileage</button>');
     body.append('<button onclick="buildPassengersInterface()">Passengers</button>');
@@ -17,23 +22,27 @@ function buildMapInterface() {
 function emptyInterface() {
     let body = $('body');
     body.empty();
-    body.append('<button onclick="buildHomeInterface()">Home</button>');
+    body.append('<button onclick="buildHomeInterface()">Home</button><br>');
 };
 
 function buildDestinationsInterface() {
     emptyInterface();
+    body.append('<h1>Destinations interface here</h1>');
 
+    //Show states/airports that model has flown in and out of + reviews for model
 };
 
 function buildMileageInterface() {
     emptyInterface();
-
+    body.append('<h1>Mileage interface here</h1>');
+    //Show airplane model mileage
 };
 
 function buildPassengersInterface() {
     emptyInterface();
+    body.append('<h1>Passengers interface here</h1>');
 
-
+    //Show reviews from passengers on airplane model?
 };
 
 function buildHomeInterface() {
