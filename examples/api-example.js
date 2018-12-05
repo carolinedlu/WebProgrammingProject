41f71c7e74aabbca75c10a1d4453dccb6be8f0f3
Roman Rogowski <rogowski@zeropointdynamics.com>
2018-12-05 16:40:49 -0500

    lat/long example

--- examples/api-example.js
+++ examples/api-example.js
@@ -1,38 +1,17 @@
-$(document).ready(() => {
-	Backend.Authenticate().done(() => {
-		console.log('Successfully authenticated!');
-		DoPlaneExample();
+$(document).ready(async () => {
+	await Backend.Authenticate().done();
+
+	// Example for latitude and longitude
+	Backend.GetPlanes().then((planes) => {
+		const plane = planes[0];
+		console.log(plane);
+		Backend.GetAirportDeparturesAndArrivalsForPlane(plane).then((flights) => {
+			for (const flight of flights) {
+				// Can also use flight.departure.latitude and flight.departure.longitude
+				const latitude = parseFloat(flight.arrival.latitude);
+				const longitude = parseFloat(flight.arrival.longitude);
+				console.log(`This plane arrived in ${flight.arrival.city} (${latitude}, ${longitude})`);
+			}
+		});
 	});
 });
-
-function DoPlaneExample() {
-	Backend.GetPlanes().then((planes) => {
-		if (planes.length === 0) {
-			alert('Database has not been seeded!');
-			return;
-		}
-
-		const plane = planes[0];
-		console.log('Here is a plane: ', plane);
-		console.log(`  Reviews (before):`, Reviews.Get(plane));
-		console.log(`  Let's fix that by adding a review...`);
-		Reviews.Add(plane, {
-			rating: 10,
-			text: "I had a wonderful experience flying this plane!",
-			name: "A. Pilot",
-		});
-		console.log(`  Reviews (after):`, Reviews.Get(plane));
-
-		Backend.UpdatePlane(plane);
-		DoYouTubeExample(plane);
-	});
-}
-
-// YouTube API query example
-function DoYouTubeExample(plane) {
-	YouTube.GetTopVideoForPlane(plane).then((link) => {
-		console.log(`Link to top video result:`, link);
-	}).catch((err) => {
-		alert(err);
-	});
-}
